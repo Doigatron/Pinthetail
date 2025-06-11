67c6b62e56df588477aa0f767dd15dcda6845114
Doigatron <r.doig@hotmail.com>
2025-06-11 13:59:49 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to PinTheTail 🐾</h1>
+      <p>Lost Pet Finder - v0.1 Scaffold</p>
+    </div>
+  )
+}
+
+export default App
