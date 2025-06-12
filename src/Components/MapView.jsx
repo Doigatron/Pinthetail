a9ba71cfacf703f006ee809473338141337a27ef
Doigatron <r.doig@hotmail.com>
2025-06-11 22:52:09 -0400

    Update MapView.jsx

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -1,25 +1,27 @@
-import React from 'react';
-import MapGL from 'react-map-gl';
-import MAPBOX_ACCESS_TOKEN from '../services/mapboxConfig';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function MapView() {
-  const [viewport, setViewport] = React.useState({
-    latitude: 43.6532, // Default to Toronto, Canada (feel free to change)
+const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
+
+const MapView = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 43.6532,
     longitude: -79.3832,
     zoom: 10
   });
 
   return (
     <div style={{ height: '90vh' }}>
-      <MapGL
+      <ReactMapGL
         {...viewport}
         width="100%"
         height="100%"
-        onViewportChange={(nextViewport) => setViewport(nextViewport)}
-        mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+        mapboxAccessToken={MAPBOX_TOKEN}
+        onMove={evt => setViewport(evt.viewState)}
       />
     </div>
   );
-}
+};
 
 export default MapView;
