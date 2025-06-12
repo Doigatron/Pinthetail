8e9d9692d6dee844cc77e97f26281f918df97b6a
Doigatron <r.doig@hotmail.com>
2025-06-11 22:54:57 -0400

    Update mapboxConfig.js

--- src/services/mapboxConfig.js
+++ src/services/mapboxConfig.js
@@ -1,4 +1,1 @@
-// Replace this with your actual Mapbox Access Token
-const MAPBOX_ACCESS_TOKEN = "YOUR_MAPBOX_ACCESS_TOKEN";
-
-export default MAPBOX_ACCESS_TOKEN;
+export const MAPBOX_TOKEN = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN;
